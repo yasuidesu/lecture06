21c5776d3844ae806ab4c35ef7d0873995d09a0b
N.Shimizu <chikoski@gmail.com>
2015-10-29 07:50:37 +0900

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var canvas;
+
+function setup(){
+  canvas = createCanvas(window.innerWidth,
+                        window.innerHeight);
+  canvas.canvas.style.height = null;
+  canvas.canvas.style.width = null;
+}
+
